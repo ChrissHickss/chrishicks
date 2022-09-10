61cfe0416bed30a69dd32724f47528ba19a98b37
ChrissHickss <chrismhicks17@gmail.com>
2022-09-09 20:31:58 -0500

    first commit

---
+++ myportfolio/feeds.py
@@ -0,0 +1,19 @@
+from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords
+from django.urls import reverse_lazy
+from .models import Post
+
+
+class LatestPostsFeed(Feed):
+    title = 'My myportfolio'
+    link = reverse_lazy('myportfolio:post_list')
+    description = 'New posts of my myportfolio.'
+
+    def items(self):
+        return Post.published.all()[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return truncatewords(item.body, 30)
