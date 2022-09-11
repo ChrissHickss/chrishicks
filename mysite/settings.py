5b1db0b99510c113b6245a17338cd185b041c91a
ChrissHickss <chrismhicks17@gmail.com>
2022-09-10 19:45:03 -0500

    new db

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,8 +83,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'my_portfolio',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
